adee61fca569b1a20dd2d8b2d1e7d26979b9fb49
chetan <chetankumar70174577@gmail.com>
2023-06-04 22:42:17 +0530

    fcdf

--- MYSQL/Cookies.php
+++ MYSQL/Cookies.php
@@ -2,7 +2,7 @@
     $link = mysqli_connect("localhost","id20612543_chetankumar","CHetan@12","id20612543_chetankumar");
     if(mysqli_connect_error())
     die("Can't connect");
-
+    // first argument of setcookie is index and secnd is value and third is time 
     // setcookie("name","chetan",time()+60*60*24);//set cookie
     // setcookie("name","",time()-60*60);//reset cookie
     // echo $_COOKIE["name"];
