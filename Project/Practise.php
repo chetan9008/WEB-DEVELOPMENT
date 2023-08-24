06bfe30ae9fba3aa7ca763ccfb2af4044a37c21f
chetan <chetankumar70174577@gmail.com>
2023-08-24 10:13:26 +0530

    new commit

---
+++ Project/Practise.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" placeholder="full name" name="name">
+        <input type="email" placeholder="Email" name="email">
+        <input type="number" placeholder="Phone number" name="phone">
+        <input type="password" placeholder="password" name="password">
+        <input type="password" name="con_password" placeholder="confirm password">
+        <button>Create account</button>
+        <p>Have a Account ? <a href="#">Log in </a></p>
+    </form>
+</body>
+</html>
